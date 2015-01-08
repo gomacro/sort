c14bcfa0e8586defd6ff54534d92130cb93d7373
anlhord <anlhord@gmail.com>
2015-01-08 14:18:38 +0100

    Insertion 32 macro

---
+++ 32/insertion/insertion.go
@@ -0,0 +1,24 @@
+package insertion
+
+func Sort(ts0 *[1]uintptr, s []uint32, compar func(*uint32, *uint32) int) {
+	incr := int((*ts0)[0])
+
+	for i := len(s) - incr; i > 0; i -= incr {
+		j := i
+		sjj := s[j-incr:]
+		sj := s[j:]
+		for j < len(s) && compar(&sj[0], &sjj[0]) < 0 {
+
+			for q := 0; q < incr; q++ {
+				x := sjj[q]
+				sjj[q] = sj[q]
+				sj[q] = x
+			}
+
+			j += incr
+			sjj = sj
+			sj = sj[incr:]
+		}
+	}
+
+}
